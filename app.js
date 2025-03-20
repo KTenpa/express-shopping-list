0bfd52925eaa8fd20c319d9ef553ca5269fd2350
Karma Tenpa <ktenpa@mac.mynetworksettings.com>
2025-03-20 17:12:05 -0500

    Set up error handling

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 const express = require("express")
 const app = express();
+const ExpressError = require("./expressError")
+
+app.use(express.json());
+
+
+// 404 handler
+app.use((req, res, next) => {
+    return new ExpressError("Not Found", 404);
+});
+
+
+// General error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.json({ error: err.message });
+});
+
+module.exports = app
